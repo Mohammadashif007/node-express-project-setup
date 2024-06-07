45bd6de7691813b4139c23e973bab23875f5146f
Mohammad Ashif <Mohammad Ashif>
2024-06-07 20:28:08 +0600

    student database

---
+++ src/app/modules/student/student.model.ts
@@ -0,0 +1,48 @@
+import { Schema, model } from 'mongoose';
+import {
+  Guardian,
+  LocalGuardian,
+  Student,
+  UserName,
+} from './student.interface';
+
+const userNameSchema = new Schema<UserName>({
+  firstName: { type: String, required: true },
+  middleName: { type: String },
+  lastName: { type: String, required: true },
+});
+
+const guardianSchema = new Schema<Guardian>({
+  fatherName: { type: String, required: true },
+  fatherOccupation: { type: String, required: true },
+  fatherContactNo: { type: String, required: true },
+});
+
+const localGuardian = new Schema<LocalGuardian>({
+  name: { type: String, required: true },
+  occupation: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  address: { type: String, required: true },
+});
+
+const studentSchema = new Schema<Student>({
+  id: { type: String, required: true },
+  name: userNameSchema,
+  gender: { type: String, enum: ['Male', 'Female'], required: true },
+  dateOfBirth: { type: String, required: true },
+  email: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  emergencyContactNo: { type: String, required: true },
+  bloodGroup: {
+    type: String,
+    enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  },
+  presentAddress: { type: String, required: true },
+  permanentAddress: { type: String, required: true },
+  guardian: guardianSchema,
+  localGuardian: localGuardian,
+  profileImage: { type: String },
+  isActive: { type: String, enum: ['active', 'block'] },
+});
+
+export const StudentModel = model<Student>('Student', studentSchema);
